16a6684f429fee9e685a098047100c8e9c14428e
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-04-24 17:48:38 -0300

    paso 6: crea el modelo alumno

--- Instituto/alumno/models.py
+++ Instituto/alumno/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
