49bac15dc8d3ce089e1c43398c5f0980e094d10b
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-04-24 17:49:24 -0300

    paso 6: crea el modelo profesor

--- Instituto/profesor/models.py
+++ Instituto/profesor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    materia = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} - {self.materia}"
