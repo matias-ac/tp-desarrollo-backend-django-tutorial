73b7373f83e8a9df177fb556f46ae214b313f1a3
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-04-24 17:51:09 -0300

    paso 6: crea el modelo cursada

--- Instituto/cursada/models.py
+++ Instituto/cursada/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from alumno.models import Alumno
+from profesor.models import Profesor
 
 # Create your models here.
+
+class Cursada(models.Model):
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    alumnos = models.ManyToManyField(Alumno, related_name="cursadas")
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cursada {self.fecha_inicio} - {self.fecha_fin} con {self.profesor}"
